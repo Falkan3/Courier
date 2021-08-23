2c51afb27e3a96a1c7d392e700ed88d430d88534
Adam Kocić <adam.kocic@hotmail.com>
2021-08-23 14:27:44 +0200

    Update: Functional tests - fix destroy test, add mount test

--- tests/functional/mount.test.js
+++ tests/functional/mount.test.js
@@ -4,16 +4,16 @@ import { query } from '../fixtures/query';
 // import defaults from '@src/defaults';
 import Courier from '../../entry/entry-complete';
 
-describe('After destroying an instance', () => {
+describe('After mounting an instance', () => {
 	beforeEach(() => {
 		document.body.innerHTML = html
 	})
 
-	test('`root element` should not exist', () => {
+	test('`root element` should exist', () => {
 		let {root} = query(document)
 
-		const instance = new Courier(root.body).mount().destroy();
+		const instance = new Courier(root.body).mount();
 
-		expect(instance.rootElement.querySelector(`.${instance._settings.classes.root}`)).toBe(null)
+		expect(instance.rootElement.querySelector(`.${instance._settings.classes.root}`)).toBeDefined()
 	})
 })
