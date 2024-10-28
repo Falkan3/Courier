6aeb367a4a0c05cb7951f40cf2c0832045cf8228
Adam Kocić <adam.kocic@hotmail.com>
2024-10-28 15:02:02 +0100

    Add noto sans font

--- src/core/inject-global-styles.js
+++ src/core/inject-global-styles.js
@@ -1,5 +1,8 @@
-import Asap400 from '../assets/fonts/asap-400.woff2';
-import Asap700 from '../assets/fonts/asap-700.woff2';
+import Asap400 from '../assets/fonts/Asap/asap-400.woff2';
+import Asap700 from '../assets/fonts/Asap/asap-700.woff2';
+import NotoSans400 from '../assets/fonts/NotoSans/noto-sans-400.woff2';
+import NotoSans400Italic from '../assets/fonts/NotoSans/noto-sans-400-italic.woff2';
+import NotoSans500 from '../assets/fonts/NotoSans/noto-sans-500.woff2';
 
 const generateFonts = () => `
 @font-face {
@@ -17,10 +20,36 @@ const generateFonts = () => `
   font-weight: 700;
   src: url(${Asap700}) format("woff2");
 }
+
+@font-face {
+  font-display: swap;
+  font-family: "Noto Sans";
+  font-style: normal;
+  font-weight: 400;
+  src: url(${NotoSans400}) format("woff2");
+}
+
+@font-face {
+  font-display: swap;
+  font-family: "Noto Sans";
+  font-style: italic;
+  font-weight: 400;
+  src: url(${NotoSans400Italic}) format("woff2");
+}
+
+@font-face {
+  font-display: swap;
+  font-family: "Noto Sans";
+  font-style: normal;
+  font-weight: 500;
+  src: url(${NotoSans500}) format("woff2");
+}
 `;
 
 // url('../src/assets/fonts/asap-400.ttf') format('truetype')
 // url('../src/assets/fonts/asap-700.ttf') format('truetype')
+// url('../src/assets/fonts/noto-sans-400.ttf') format('truetype')
+// url('../src/assets/fonts/noto-sans-700.ttf') format('truetype')
 
 export function injectGlobalStyles() {
     const style = document.createElement('style');
